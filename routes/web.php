646809e236c1b8ec8fae884def68cd6f2d0d68e5
Ric Le Poidevin <ric@uandus.co.uk>
2025-09-15 16:41:59 +0100

    Add route name for `laravel-storyblok-icons` endpoint

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,5 @@
 
 use Riclep\LaravelStoryblokIcons\Http\Controllers\LaravelStoryblokIconsController;
 
-Route::get('laravel-storyblok-icons', [LaravelStoryblokIconsController::class, 'show']);
+Route::get('laravel-storyblok-icons', [LaravelStoryblokIconsController::class, 'show'])->name('ls-icon');
 Route::get('laravel-storyblok-icons.json', [LaravelStoryblokIconsController::class, 'json']);
